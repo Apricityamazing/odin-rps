d49088a5ab4573ddc2926aff17461ee8658aa3d0
Apricityamazing <apricityamazing@gmail.com>
2025-07-29 14:25:27 -0500

    Added the getHumanChoice function

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,4 +1,4 @@
-function getcomputerchoice(numberofchoices) {
+function getComputerChoice(numberofchoices) {
  let choice = Math.floor(Math.random() * numberofchoices);
  if (choice == 0) {
    return "Rock!";
@@ -10,4 +10,21 @@ function getcomputerchoice(numberofchoices) {
     return "Scissors!";
  } 
 }
-console.log(getcomputerchoice(3));
+
+function getHumanChoice() {
+    let choice = window.prompt("Choose one! Rock, Paper, or Scissors?");
+    if (choice == "Rock") {
+        return "Rock!"
+    }
+    else if (choice == "Paper") {
+        return "Paper!";
+    }
+    else if (choice == "Scissors") {
+        return "Scissors"
+    }
+    else {
+        console.log("Invalid response. Make sure to capitalize the first letter.");
+        getHumanChoice();
+    }
+}
+getHumanChoice();
